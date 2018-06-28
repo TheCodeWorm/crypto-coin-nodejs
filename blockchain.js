adcd5db486512fa15c0181b67458a1c26f333ab0
TheCodeWorm <4purposeliving@gmail.com>
2018-06-27 19:24:12 -0700

    updates

--- blockchain.js
+++ blockchain.js
@@ -2,23 +2,31 @@
 class Blockchain {
   constructor () {
     // Create chain and transaction
-    this.chain = []
-    this.current_transactions = []
+    this.chain = [];
+    this.current_transactions = [];
 
     // Binding of this
-    this.newBlock = this.newBlock.bind(this)
-    this.newTransaction = this.newTransaction.bind(this)
-    this.lastBlock = this.lastBlock.bind(this)
-    this.proofOfWork = this.proofOfWork.bind(this)
+    this.newBlock = this.newBlock.bind(this);
+    this.newTransaction = this.newTransaction.bind(this);
+    this.lastBlock = this.lastBlock.bind(this);
+    this.proofOfWork = this.proofOfWork.bind(this);
   }
 
-  newBlock () { /* Create the new block */ }
+  newBlock() { 
+    /* Create the new block */ 
+  }
 
-  newTransaction () { /* Store a new transaction */ }
+  newTransaction() { 
+    /* Store a new transaction */ 
+  }
 
-  hash (block) { /* hash the block */ }
+  hash(block) { 
+    /* hash the block */ 
+  }
 
-  lastBlock () { /* return the last block */}
+  lastBlock() { 
+    /* return the last block */
+  }
 }
 
 module.exports = Blockchain;
